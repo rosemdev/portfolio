90f69720db6e02f58f1c9a25cb32775f049d55b7
RomSem <romasemenyshyn@gmail.com>
2018-08-16 22:12:45 +0300

    Moved theme changing feature to the separate component.

--- src/components/GradientScreen.vue
+++ src/components/GradientScreen.vue
@@ -7,28 +7,19 @@
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci deserunt dignissimos dolorem eaque
                     est facere laudantium magnam maiores, numquam quia quibusdam sequi, sit vero voluptas?</p>
             </div>
-            <div class="theme">
-                <p class="check">Check it out</p>
-                <div class="select-theme">
-                    <span @click="$root.$emit('change-theme', 'theme1')">Click me</span>
-                    <span @click="$root.$emit('change-theme', 'theme2')">...or me</span>
-                    <span @click="$root.$emit('change-theme', 'theme3')">meeee!!</span>
-                </div>
-                <p class="reset" @click="$root.$emit('change-theme', 'theme-default')">reset</p>
-            </div>
+            <rosem-theme></rosem-theme>
             <slot name="mainPage"></slot>
         </div>
     </div>
 </template>
 
 <script>
-    export default {
-        data() {
-            return {}
-        },
+    import RosemTheme from "../components/Theme"
 
-        computed: {},
-        methods: {},
+    export default {
+        components: {
+            RosemTheme
+        }
     }
 
 </script>
@@ -72,72 +63,6 @@
             }
 
         }
-
-        & .theme {
-            p {
-                color: white;
-                font-size: 1.5em;
-                font-weight: 100;
-
-                &.check {
-                    position: relative;
-
-                    &:hover::after {
-                        top: 35px;
-                    }
-
-                    &:after {
-                        content: "";
-                        border: solid white;
-                        border-width: 0 2px 2px 0;
-                        display: inline-block;
-                        padding: 4px;
-                        transform: rotate(45deg);
-                        position: absolute;
-                        left: 50%;
-                        right: 50%;
-                        top: 30px;
-                    }
-                }
-
-                &.reset {
-                    font-size: 1.2rem;
-                    cursor: pointer;
-                }
-            }
-
-            & .select-theme {
-                display: flex;
-                align-items: center;
-                justify-content: center;
-
-            }
-
-            & span {
-                width: 80px;
-                height: 80px;
-                border-radius: 50%;
-                margin: 15px;
-                color: white;
-                text-align: center;
-                font-size: 1em;
-                line-height: 80px;
-                cursor: pointer;
-                box-shadow: 0 3px 20px 3px #0000004d;
-
-                &:first-child {
-                    background: @theme1;
-                }
-
-                &:nth-child(2) {
-                    background: @theme2;
-                }
-
-                &:last-child {
-                    background: @theme3;
-                }
-            }
-        }
     }
 
     .responsive(@tablet, { .gradient {
@@ -166,18 +91,6 @@
                 font-size: 2.7rem;
             }
         }
-
-        & .theme {
-            & .select-theme {
-                margin: 2.5rem 0;
-                & span {
-                    width: 100px;
-                    height: 100px;
-                    line-height: 100px;
-                }
-            }
-        }
-
     } });
 
 </style>
---
+++ src/components/Theme.vue
@@ -0,0 +1,93 @@
+<template>
+    <div class="theme">
+        <p class="check">Check it out</p>
+        <div class="select-theme">
+            <span @click="$root.$emit('change-theme', 'theme1')">Click me</span>
+            <span @click="$root.$emit('change-theme', 'theme2')">...or me</span>
+            <span @click="$root.$emit('change-theme', 'theme3')">meeee!!</span>
+        </div>
+        <p class="reset" @click="$root.$emit('change-theme', 'theme-default')">reset</p>
+    </div>
+</template>
+<style lang="less" scoped>
+    @import "../assets/styles/globalVariables";
+    @import "../assets/styles/mixins";
+    @import "../assets/styles/themes";
+
+    & .theme {
+        p {
+            color: white;
+            font-size: 1.5em;
+            font-weight: 100;
+
+            &.check {
+                position: relative;
+
+                &:hover::after {
+                    top: 35px;
+                }
+
+                &:after {
+                    content: "";
+                    border: solid white;
+                    border-width: 0 2px 2px 0;
+                    display: inline-block;
+                    padding: 4px;
+                    transform: rotate(45deg);
+                    position: absolute;
+                    left: 50%;
+                    right: 50%;
+                    top: 30px;
+                }
+            }
+
+            &.reset {
+                font-size: 1.2rem;
+                cursor: pointer;
+            }
+        }
+
+        & .select-theme {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+
+        }
+
+        & span {
+            width: 80px;
+            height: 80px;
+            border-radius: 50%;
+            margin: 15px;
+            color: white;
+            text-align: center;
+            font-size: 1em;
+            line-height: 80px;
+            cursor: pointer;
+            box-shadow: 0 3px 20px 3px #0000004d;
+
+            &:first-child {
+                background: @theme1;
+            }
+
+            &:nth-child(2) {
+                background: @theme2;
+            }
+
+            &:last-child {
+                background: @theme3;
+            }
+        }
+    }
+
+    .responsive(@desktop, { .theme {
+        & .select-theme {
+            margin: 2.5rem 0;
+            & span {
+                width: 100px;
+                height: 100px;
+                line-height: 100px;
+            }
+        }
+    } });
+</style>
