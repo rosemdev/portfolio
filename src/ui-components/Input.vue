6e1070919c5692a2e964725cb01f1cc5fbe14ba0
RomSem <romasemenyshyn@gmail.com>
2018-09-02 22:52:08 +0300

    Created Input component.

--- src/ui-components/Input.vue
+++ src/ui-components/Input.vue
@@ -1,8 +1,134 @@
 <template>
-    <div>
-        <input/>
+    <div class="input">
+        <label :for="_uid"><span><slot></slot></span></label>
+        <input
+                ref="input"
+                v-bind="$attrs"
+                :id=" _uid"
+                @blur="checkValidity($event.target)"
+                @invalid.prevent="valid = false"
+        />
+        <p v-if="!valid">
+            <span class="error">{{$refs.input.validationMessage}}</span>
+        </p>
     </div>
 </template>
 <script>
+    import {defaultValidationErrors} from "../data/defaultValidationErrors"
 
+    const VALIDATION_TYPES = [
+        'badInput',
+        'patternMismatch',
+        'rangeOverflow',
+        'rangeUnderflow',
+        'stepMismatch',
+        'tooLong',
+        'tooShort',
+        'typeMismatch',
+        'valueMissing',
+    ];
+
+    export default {
+        inheritAttrs: false,
+        inject: ['fields'],
+
+        data() {
+            return {
+                valid: true
+            }
+        },
+
+        props: {
+            error: {
+                type: String,
+
+            }
+        },
+
+        methods: {
+            checkValidity(input) {
+                this.valid = true;
+                input.setCustomValidity('');
+                input.checkValidity();
+
+                for (let type of VALIDATION_TYPES) {
+                    if (input.validity[type]) {
+                        input.setCustomValidity(defaultValidationErrors[type](input));
+                    }
+                }
+
+                return input.validity.valid;
+            }
+        },
+
+
+        mounted() {
+            this.fields.push(this);
+        },
+    }
 </script>
+<style lang="less" scoped>
+    @import "../assets/styles/design";
+    @import "../assets/styles/mixins";
+
+    .input {
+        position: relative;
+        display: flex;
+        align-items: start;
+        justify-content: start;
+
+    }
+
+    label {
+        display: block;
+        top: 9px;
+        margin: 10px 0;
+        position: absolute;
+        left: 9px;
+        overflow: hidden;
+        width: 275px;
+        height: 50px;
+        text-align: left;
+        font-size: 15px;
+
+        &:before{
+            content: '';
+            position: absolute;
+            height: 100%;
+            background-color: @mainColor;
+            width: 1px;
+            top: 16px;
+
+        }
+
+        &:after{
+            content: '';
+            position: absolute;
+            width: 100%;
+            background-color: @mainColor;
+            height: 1px;
+            top: 6px;
+            margin-left: 5px;
+
+        }
+
+    }
+
+    input {
+        border-bottom: 1px solid @mainColor;
+        border-right: 1px solid @mainColor;
+        padding: 10px;
+        display: block;
+        margin: 25px 10px;
+        font-size: 20px;
+        width: 275px;
+        color: @mainColor;
+        background-color: inherit;
+        box-shadow: 0 0.3vw 3vw -0.7vw rgba(0, 0, 0, 0.2);
+
+        &::placeholder {
+            font-size: 15px;
+        }
+    }
+
+</style>
