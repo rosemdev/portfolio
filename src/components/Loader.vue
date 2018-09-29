f2e9b67cf82cd0baaf1bcaa0cb1d6f2896c12042
RomSem <romasemenyshyn@gmail.com>
2018-09-29 22:19:24 +0300

    Created Loader component.

---
+++ src/components/Loader.vue
@@ -0,0 +1,87 @@
+<template>
+    <div class="lds-ellipsis">
+        <div :style="{backgroundColor: color}"></div>
+        <div :style="{backgroundColor: color}"></div>
+        <div :style="{backgroundColor: color}"></div>
+        <div :style="{backgroundColor: color}"></div>
+    </div>
+</template>
+<script>
+    export default {
+        data() {
+            return {}
+        },
+
+        props: {
+            color: {
+                type: String,
+                default: '#333'
+            }
+        }
+    }
+</script>
+<style lang="less" scoped>
+    .lds-ellipsis {
+        display: inline-block;
+        position: relative;
+        width: 64px;
+        height: 64px;
+
+        & div {
+            position: absolute;
+            top: 27px;
+            width: 11px;
+            height: 11px;
+            border-radius: 50%;
+            animation-timing-function: cubic-bezier(0, 1, 1, 0);
+
+            &:nth-child(1) {
+                left: 6px;
+                animation: lds-ellipsis1 0.6s infinite;
+            }
+
+            &:nth-child(2) {
+                left: 6px;
+                animation: lds-ellipsis2 0.6s infinite;
+            }
+
+            &:nth-child(3) {
+                left: 26px;
+                animation: lds-ellipsis2 0.6s infinite;
+            }
+
+            &:nth-child(4) {
+                left: 45px;
+                animation: lds-ellipsis3 0.6s infinite;
+            }
+        }
+    }
+
+    @keyframes lds-ellipsis1 {
+        0% {
+            transform: scale(0);
+        }
+        100% {
+            transform: scale(1);
+        }
+    }
+
+    @keyframes lds-ellipsis3 {
+        0% {
+            transform: scale(1);
+        }
+        100% {
+            transform: scale(0);
+        }
+    }
+
+    @keyframes lds-ellipsis2 {
+        0% {
+            transform: translate(0, 0);
+        }
+        100% {
+            transform: translate(19px, 0);
+        }
+    }
+
+</style>
