cba3f39a9a26fe0379a49707b2f7da5da838d43d
RomSem <romasemenyshyn@gmail.com>
2018-11-21 20:18:14 +0200

    Added pre-rendering only for production.

--- vue.config.js
+++ vue.config.js
@@ -1,20 +1,8 @@
 const path = require('path');
 const PrerenderSPAPlugin = require('prerender-spa-plugin');
 const PuppeteerRenderer = PrerenderSPAPlugin.PuppeteerRenderer;
-
-
-
-module.exports = {
-    configureWebpack: {
-        resolve: {
-            alias: {
-                '@store': path.join(__dirname, 'src/store'),
-                'vue$': 'vue/dist/vue.esm.js' // 'vue/dist/vue.common.js' for webpack 1
-            }
-        },
-
-        plugins: [
-                new PrerenderSPAPlugin({
+const productionPlugins = [
+    new PrerenderSPAPlugin({
                     // Required - The path to the webpack-outputted app to prerender.
                     staticDir: path.join(__dirname, 'dist'),
                     // Required - Routes to render.
@@ -22,8 +10,20 @@ module.exports = {
                     renderer: new PuppeteerRenderer({
                         renderAfterElementExists: '#app'
                     }),
-                })
-        ]
+                }),
+];
 
-    }
+
+
+module.exports = {
+    configureWebpack: (config) => {
+        if (process.env.NODE_ENV === 'production') {
+            config.plugins.push(...productionPlugins);
+        }
+
+        Object.assign(config.resolve.alias, {
+            '@store': path.join(__dirname, 'src/store'),
+            'vue$': 'vue/dist/vue.esm.js' // 'vue/dist/vue.common.js' for webpack 1
+        });
+    },
 };
