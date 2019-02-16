6f09df4d3f15e07d5d72faeb3c39036c069deee6
RomSem <romasemenyshyn@gmail.com>
2019-02-16 10:26:09 +0200

    Redesign changes. #41 Changed logo and logo component.

--- src/partials/Logo.vue
+++ src/partials/Logo.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="logo">
         <router-link to="/">
-            <span>R.<sup>osem</sup></span>
+            <img src="../assets/images/icons/logo.png" alt="rosem_logo">
         </router-link>
     </div>
 </template>
@@ -16,67 +16,12 @@
     .logo {
         height: 50px;
         width: 50px;
-        background-color: @mainColor;
         margin: 15px 5px;
-        transition: all .5s;
-        position: relative;
 
-        & a {
-            text-align: center;
-            font-size: 35px;
-            font-weight: 900;
-            font-family: 'Metamorphous', cursive;
-            display: flex;
-            justify-content: flex-start;
-            align-items: flex-start;
-            color: white;
-
-            span {
-                margin-left: 5px;
-
-                & sup {
-                    font-size: 11px;
-                    margin-left: -10px;
-                    font-weight: 400;
-                    transition: opacity .5s ease-out;
-                    opacity: 0;
-                }
-            }
+        img {
+            width: 100%;
         }
     }
 
-    .header.sticky {
-        & header {
-            .logo {
-                margin: 0;
-
-                & a {
-                    span {
-                        sup {
-                            display: inline;
-                            opacity: 1;
-                        }
-                    }
-                }
-
-            }
-
-        }
-    }
-
-    .responsive(@tablet, { .logo {
-        & a {
-            font-size: 35px;
-            & span {
-                margin-top: 4px;
-                margin-left: 5px;
-
-                sup {
-                    display: none;
-                }
-            }
-
-        }
-    } });
 
 </style>
