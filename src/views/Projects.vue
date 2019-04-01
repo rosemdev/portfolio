cfb567a932f1dca1162f1d8a4cda9ffbb4344a32
RomSem <romasemenyshyn@gmail.com>
2019-04-02 00:38:34 +0300

    Project page changes.

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,107 +1,100 @@
 <template>
     <div class="projects">
         <div class="first-project">
-            <div class="main-content">
-                <p class="section-title hashtag left-text" data-hashtag="landing">Mobile app</p>
-            </div>
             <div class="main-content overview">
-                <div class="mobile-app-description">
-                    <p class="short-description left-text">
+                <p class="section-title hashtag left-text" data-hashtag="landing">Mobile app</p>
+                <div class="mobile-app-description right-text">
+                    <p class="short-description">
                         This is not exactly the first my learning project, but it's the first
                         not ugly ;) and more beautiful than previous. As you know - learning is a hard work, especially
                         programing. So here I going to describe some concepts of this projects, but I must confess - the
                         design - it's not my idea. I love Photoshop, I could doing some works related into
                         photocorrection
-                        and some cool effects, but website design development - it's not my case unfortunately.
+                        and some cool effects, but website design development - it's not my case unfortunately. I hope
+                        there there more creative people who can make masterpieces.
                     </p>
                 </div>
-                <div class="mobile-app">
-                    <img src="../assets/images/myWorks/2019-01-20_01-01.png" alt="">
-                </div>
             </div>
+            <div class="mobile-app"></div>
             <div class="grey-background">
                 <div class="main-content about">
-                    <p class="section-title hashtag-right right-text" data-hashtag="about">What? When? Why?</p>
-                    <p class="short-description right-text">So after long search of something interesting, I forgot
-                        about
-                        programing and hung out on different resources about art, creativity and mastery of people over
-                        the
-                        world - such as
-                        <a class="link" href="https://behance.net" target="_blank" rel="noopener">behance.net</a>,
-                        <a
-                                class="link" href="https://land-book.com/" target="_blank"
-                                rel="noopener">land-book.com</a>,
-                        <a
-                                class="link"
-                                href="https://bashooka.com/" target="_blank" rel="noopener">bashooka.com</a>,
-                        <a
-                                class="link"
-                                href="https://dribbble.com/" target="_blank" rel="noopener">dribbble.com</a> and of
-                        course
-                        <a
-                                class="link"
-                                href="https://www.pinterest.com/" target="_blank" rel="noopener">pinterest.com</a> and
-                        finally found
-                        something to test my new skills. So I found this site so cute to learn and feel happy to love
-                        what
-                        you're doing. Seems it's a landing page of some mobile app for iPhone.
-                    </p>
-                    <p class="short-description right-text">Please take a look into
-                        site itself, also follow the link to gitHub.
-                    <ul>
-                        <li><a href="https://romsem.github.io/mobapp/" class="link" target="_blank" rel="noopener">mobapp</a>
-                        </li>
-                        <li><a href="https://github.com/RomSem/mobapp" class="link" target="_blank" rel="noopener">the
-                            repo in gitHub</a></li>
-                    </ul>
-                    </p>
-                </div>
-                <div class="main-content color-palette">
-                    <p class="section-title hashtag" data-hashtag="colors">Color palette</p>
-                    <p class="short-description left-text">I love a bright colors so much, so this design has subdued
-                        me especially in conjunction with a gradient.</p>
-                    <div class="colors">
-                        <span>#4b0e8f</span>
-                        <span>#f6298f</span>
-                        <span>#faf6fb</span>
+                    <div class="gif-code">
+                        <img src="../assets/images/hurray.gif" alt="">
                     </div>
-                    <div>
+                    <div class="project-description">
+                        <p class="section-title hashtag-right right-text" data-hashtag="about">What? When? Why?</p>
+                        <p class="short-description right-text">So after long search of something interesting, I forgot
+                            about
+                            programing and hung out on different resources about art, creativity and mastery of people
+                            over
+                            the
+                            world - such as
+                            <a class="link" href="https://behance.net" target="_blank" rel="noopener">behance.net</a>,
+                            <a
+                                    class="link" href="https://land-book.com/" target="_blank"
+                                    rel="noopener">land-book.com</a>,
+                            <a
+                                    class="link"
+                                    href="https://bashooka.com/" target="_blank" rel="noopener">bashooka.com</a>,
+                            <a
+                                    class="link"
+                                    href="https://dribbble.com/" target="_blank" rel="noopener">dribbble.com</a> and of
+                            course
+                            <a
+                                    class="link"
+                                    href="https://www.pinterest.com/" target="_blank" rel="noopener">pinterest.com</a>
+                            and
+                            finally found
+                            something to test my new skills. So I found this site so cute to learn and feel happy to
+                            love
+                            what
+                            you're doing. Seems it's a landing page of some mobile app for iPhone.
+                        </p>
+                        <p class="short-description right-text">Please take a look into
+                            site itself, also follow the link to gitHub.
+                        <ul>
+                            <li><a href="https://romsem.github.io/mobapp/" class="link" target="_blank" rel="noopener">mobapp</a>
+                            </li>
+                            <li><a href="https://github.com/RomSem/mobapp" class="link" target="_blank" rel="noopener">the
+                                repo in gitHub</a></li>
+                        </ul>
+                        </p>
+                        <p class="section-title hashtag right-text" data-hashtag="colors">Color palette</p>
+                        <p class="short-description right-text">I love a bright colors so much, so this design has
+                            subdued
+                            me especially in conjunction with a gradient.</p>
+                        <div class="colors">
+                            <span>#4b0e8f</span>
+                            <span>#f6298f</span>
+                            <span>#faf6fb</span>
+                        </div>
                     </div>
                 </div>
             </div>
+        </div>
+        <div class="second-project">
+            <div class="main-content">
+                <p class="section-title hashtag left-text" data-hashtag="traveling">Travel Agency</p>
+            </div>
+            <div class="travel-agency"></div>
             <div>
-                <p class="section-title hashtag-right right-text" data-hashtag="design">Screenshots</p>
-                <div class="screenshots">
-                    <div class="image-holder top">
-                        <img src="../assets/images/myWorks/mobApp1.png" alt="">
-                    </div>
-                    <div class="image-holder">
-                        <img src="../assets/images/myWorks/mobApp2.png" alt="">
-                    </div>
-                    <div class="main-content top">
-                        <p class="section-title hashtag-right left-text" data-hashtag="interacting">Look and Feel</p>
-                        <p class="short-description left-text">To be more attractive the landing page should have some
-                            thing with a user could "touch" a long story short it's how he feel when use this site and
-                            what he will do next. I love to use a lot of hover effects, shadows (maybe you have already
-                            noticed that), animations also different
-                            types of ways submitting the content means tables, accordions, carousels - all this actions
-                            directed on user attention. So The "look" is defined by the following components:
-                        <ol>
-                            <li>colors</li>
-                            <li>images</li>
-                            <li>layout</li>
-                            <li>font family</li>
-                            <li>whole designer flavor</li>
-                        </ol>
-
-                        </p>
-                    </div>
-                    <div class="image-holder">
-                        <img src="../assets/images/myWorks/mobApp4.png" alt="">
-                    </div>
-                    <div class="image-holder top">
-                        <img src="../assets/images/myWorks/mobApp3.png" alt="">
-                    </div>
+                <div class="main-content top">
+                    <p class="section-title hashtag-right right-text" data-hashtag="interacting">Look and Feel</p>
+                    <p class="short-description left-text">To be more attractive the landing page should have some
+                        thing with a user could "touch" a long story short it's how he feel when use this site and
+                        what he will do next.
+                        I love to use a lot of hover effects, shadows (maybe you have already
+                        noticed that), animations also different
+                        types of ways submitting the content means tables, accordions, carousels - all this actions
+                        directed on user attention. So The "look" is defined by the following components:
+                    <ol>
+                        <li>colors</li>
+                        <li>images</li>
+                        <li>layout</li>
+                        <li>font family</li>
+                        <li>whole designer flavor</li>
+                    </ol>
+                    </p>
                 </div>
             </div>
         </div>
@@ -152,23 +145,49 @@
             .overview {
                 display: flex;
                 align-items: center;
-                justify-content: space-between;
-                flex-direction: column;
+                justify-content: center;
+
+                .section-title {
+                    &:before {
+                        z-index: -1;
+                    }
+                }
 
                 .mobile-app-description {
                     width: 100%;
-                    max-width: 400px;
+                    max-width: 800px;
+                    margin: auto;
+                    padding: 15px 50px;
+                    z-index: 2;
+                }
+
+            }
+
+            .mobile-app {
+                background-image: url("../assets/images/myWorks/mobile-app.png");
+                width: 100%;
+                height: 750px;
+                background-attachment: fixed;
+                background-repeat: no-repeat;
+                background-size: cover;
+                box-shadow: 0 1.8vw 4vw -0.7vw rgba(0, 0, 0, 0.2);
+                position: relative;
+            }
+
+            .about {
+                display: flex;
+                align-items: center;
+                justify-content: center;
+
+                .gif-code {
+                    margin-top: -19rem;
+                    z-index: 3;
+                }
+
+                .project-description {
                     margin: 25px;
                 }
 
-                .mobile-app {
-                    img {
-                        width: 100%;
-                    }
-                }
-            }
-
-            .color-palette {
                 .colors {
                     display: flex;
                     align-items: center;
@@ -185,6 +204,11 @@
                         cursor: pointer;
                         box-shadow: 0 3px 20px 3px #0000004d;
                         flex-shrink: 0;
+                        transition: transform .3s ease-in-out;
+
+                        &:hover {
+                            transform: scale(.9);
+                        }
 
                         &:first-child {
                             background-color: #4b0e8f;
@@ -203,36 +227,26 @@
                 }
             }
 
-            .screenshots {
-                display: flex;
-                justify-content: space-between;
-                flex-direction: row;
-                flex-wrap: wrap;
+        }
 
-                .top {
-                    margin-top: -140px;
-                }
-
-                .image-holder {
-                    flex-basis: 49%;
-                    margin-right: 10px;
-
-                    img {
-                        width: 100%;
-                        max-width: 900px;
-                        display: block;
-                    }
-
-                }
+        .second-project {
+            .travel-agency {
+                background-image: url("../assets/images/myWorks/travel_agency.png");
+                width: 100%;
+                height: 900px;
+                background-attachment: fixed;
+                background-repeat: no-repeat;
+                background-size: contain;
+                box-shadow: 0 1.8vw 4vw -0.7vw rgba(0, 0, 0, 0.2);
+                position: relative;
             }
-
         }
     }
 
     .responsive(@tablet, { .projects {
         .first-project {
             .overview {
-                flex-direction: row;
+
             }
         }
     } });
