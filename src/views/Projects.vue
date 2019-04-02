704a8fd57c69117ea6d765f0821a90cb8ca01a0e
RomSem <romasemenyshyn@gmail.com>
2019-04-02 23:33:32 +0300

    Project page.

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -4,18 +4,28 @@
             <div class="main-content overview">
                 <p class="section-title hashtag left-text" data-hashtag="landing">Mobile app</p>
                 <div class="mobile-app-description right-text">
-                    <p class="short-description">
-                        This is not exactly the first my learning project, but it's the first
-                        not ugly ;) and more beautiful than previous. As you know - learning is a hard work, especially
-                        programing. So here I going to describe some concepts of this projects, but I must confess - the
-                        design - it's not my idea. I love Photoshop, I could doing some works related into
-                        photocorrection
-                        and some cool effects, but website design development - it's not my case unfortunately. I hope
-                        there there more creative people who can make masterpieces.
-                    </p>
+                    <rosem-description-block subtitle=""
+                                             title="">
+                        <template slot="description">
+                            <p class="short-description">
+                                This is not exactly the first my learning project, but it's the first
+                                not ugly ;) and more beautiful than previous. As you know - learning is a hard work,
+                                especially
+                                programing. So here I going to describe some concepts of this projects, but I must
+                                confess -
+                                the
+                                design - it's not my idea. I love Photoshop, I could doing some works related into
+                                photocorrection
+                                and some cool effects, but website design development - it's not my case unfortunately.
+                                I
+                                hope
+                                there there more creative people who can make masterpieces.
+                            </p>
+                        </template>
+                    </rosem-description-block>
                 </div>
             </div>
-            <div class="mobile-app"></div>
+            <div class="mobile-app parallax"></div>
             <div class="grey-background">
                 <div class="main-content about">
                     <div class="gif-code">
@@ -23,51 +33,65 @@
                     </div>
                     <div class="project-description">
                         <p class="section-title hashtag-right right-text" data-hashtag="about">What? When? Why?</p>
-                        <p class="short-description right-text">So after long search of something interesting, I forgot
-                            about
-                            programing and hung out on different resources about art, creativity and mastery of people
-                            over
-                            the
-                            world - such as
-                            <a class="link" href="https://behance.net" target="_blank" rel="noopener">behance.net</a>,
-                            <a
-                                    class="link" href="https://land-book.com/" target="_blank"
-                                    rel="noopener">land-book.com</a>,
-                            <a
-                                    class="link"
-                                    href="https://bashooka.com/" target="_blank" rel="noopener">bashooka.com</a>,
-                            <a
-                                    class="link"
-                                    href="https://dribbble.com/" target="_blank" rel="noopener">dribbble.com</a> and of
-                            course
-                            <a
-                                    class="link"
-                                    href="https://www.pinterest.com/" target="_blank" rel="noopener">pinterest.com</a>
-                            and
-                            finally found
-                            something to test my new skills. So I found this site so cute to learn and feel happy to
-                            love
-                            what
-                            you're doing. Seems it's a landing page of some mobile app for iPhone.
-                        </p>
-                        <p class="short-description right-text">Please take a look into
-                            site itself, also follow the link to gitHub.
-                        <ul>
-                            <li><a href="https://romsem.github.io/mobapp/" class="link" target="_blank" rel="noopener">mobapp</a>
-                            </li>
-                            <li><a href="https://github.com/RomSem/mobapp" class="link" target="_blank" rel="noopener">the
-                                repo in gitHub</a></li>
-                        </ul>
-                        </p>
-                        <p class="section-title hashtag right-text" data-hashtag="colors">Color palette</p>
-                        <p class="short-description right-text">I love a bright colors so much, so this design has
-                            subdued
-                            me especially in conjunction with a gradient.</p>
-                        <div class="colors">
-                            <span>#4b0e8f</span>
-                            <span>#f6298f</span>
-                            <span>#faf6fb</span>
-                        </div>
+                        <rosem-description-block subtitle=""
+                                                 title="">
+                            <template slot="description">
+                                <p class="short-description right-text">So after long search of something interesting, I
+                                    forgot
+                                    about
+                                    programing and hung out on different resources about art, creativity and mastery of
+                                    people
+                                    over
+                                    the
+                                    world - such as
+                                    <a class="link" href="https://behance.net" target="_blank" rel="noopener">behance.net</a>,
+                                    <a
+                                            class="link" href="https://land-book.com/" target="_blank"
+                                            rel="noopener">land-book.com</a>,
+                                    <a
+                                            class="link"
+                                            href="https://bashooka.com/" target="_blank" rel="noopener">bashooka.com</a>,
+                                    <a
+                                            class="link"
+                                            href="https://dribbble.com/" target="_blank" rel="noopener">dribbble.com</a>
+                                    and of
+                                    course
+                                    <a
+                                            class="link"
+                                            href="https://www.pinterest.com/" target="_blank" rel="noopener">pinterest.com</a>
+                                    and
+                                    finally found
+                                    something to test my new skills. So I found this site so cute to learn and feel
+                                    happy to
+                                    love
+                                    what
+                                    you're doing. Seems it's a landing page of some mobile app for iPhone.
+                                </p>
+                                <div>
+                                    <p class="short-description right-text">Please take a look into
+                                        site itself, also follow the link to gitHub.
+                                    </p>
+                                    <ul class="right-text">
+                                        <li><a href="https://romsem.github.io/mobapp/" class="link" target="_blank"
+                                               rel="noopener">mobapp</a>
+                                        </li>
+                                        <li><a href="https://github.com/RomSem/mobapp" class="link" target="_blank"
+                                               rel="noopener">the
+                                            repo in gitHub</a></li>
+                                    </ul>
+                                    <p class="short-description right-text">I love a bright colors so much, so this
+                                        design
+                                        has
+                                        subdued
+                                        me especially in conjunction with a gradient.</p>
+                                    <div class="colors">
+                                        <span>#4b0e8f</span>
+                                        <span>#f6298f</span>
+                                        <span>#faf6fb</span>
+                                    </div>
+                                </div>
+                            </template>
+                        </rosem-description-block>
                     </div>
                 </div>
             </div>
@@ -75,10 +99,41 @@
         <div class="second-project">
             <div class="main-content">
                 <p class="section-title hashtag left-text" data-hashtag="traveling">Travel Agency</p>
+                <rosem-description-block subtitle=""
+                                         title=""
+                >
+                    <template slot="description">
+                        <p class="short-description left-text">
+                            This is a small project made on the basis of Udemy course.
+                            <a href="https://github.com/LearnWebCode" class="link" target="_blank"
+                               rel="noopener">Brad Schiff</a> is a great teacher, and this course may become a brilliant
+                            find for the
+                            beginner. The amazing part of this course is instructor tells you some
+                            amazing tips and tricks you can use to make your work easier and faster.
+                            At this project I firstly met a Webpack and started to configure it, come to know some new
+                            for me JavaScript features and even CSS and HTML, about proper style classes organization
+                            and so on... Please find below the link to get detailed information:
+                        </p>
+                        <ul class="left-text">
+                            <li><a href="https://romsem.github.io/travelAgency/" class="link" target="_blank"
+                                   rel="noopener">The travel agency</a>
+                            </li>
+                            <li><a href="https://github.com/RomSem/travelAgency" class="link" target="_blank"
+                                   rel="noopener">the
+                                repo in gitHub</a></li>
+                        </ul>
+                    </template>
+                </rosem-description-block>
             </div>
-            <div class="travel-agency"></div>
+            <div class="travel-agency parallax"></div>
+            <div class="third-project">
+                <div class="main-content">
+                    <p class="section-title hashtag-right left-text" data-hashtag="Tasks">To do list</p>
+                </div>
+<!--                <div class="todo-list parallax"></div>-->
+            </div>
             <div>
-                <div class="main-content top">
+                <div class="main-content">
                     <p class="section-title hashtag-right right-text" data-hashtag="interacting">Look and Feel</p>
                     <p class="short-description left-text">To be more attractive the landing page should have some
                         thing with a user could "touch" a long story short it's how he feel when use this site and
@@ -103,6 +158,7 @@
 <script>
     import RosemButton from "../ui-components/Button"
     import RosemQuote from "../components/Quote";
+    import RosemDescriptionBlock from "../components/DescriptionBlock";
 
 
     export default {
@@ -120,6 +176,7 @@
         components: {
             RosemButton,
             RosemQuote,
+            RosemDescriptionBlock
         },
     }
 </script>
@@ -131,11 +188,18 @@
     .short-description {
         line-height: 32px;
         font-size: 19px;
-        color: @mainColor
     }
 
     ol {
         list-style: disc;
+    }
+
+    .parallax {
+        width: 100%;
+        background-attachment: fixed;
+        background-repeat: no-repeat;
+        box-shadow: 0 1.8vw 4vw -0.7vw rgba(0, 0, 0, 0.2);
+        position: relative;
     }
 
     .projects {
@@ -177,54 +241,56 @@
             .about {
                 display: flex;
                 align-items: center;
-                justify-content: center;
+                justify-content: flex-start;
 
                 .gif-code {
-                    margin-top: -19rem;
+                    position: relative;
+                    top: -8rem;
                     z-index: 3;
                 }
 
                 .project-description {
                     margin: 25px;
-                }
 
-                .colors {
-                    display: flex;
-                    align-items: center;
+                    .colors {
+                        display: flex;
+                        align-items: center;
 
-                    & span {
-                        width: 80px;
-                        height: 80px;
-                        border-radius: 50%;
-                        margin: 15px;
-                        color: white;
-                        text-align: center;
-                        font-size: 1em;
-                        line-height: 80px;
-                        cursor: pointer;
-                        box-shadow: 0 3px 20px 3px #0000004d;
-                        flex-shrink: 0;
-                        transition: transform .3s ease-in-out;
+                        & span {
+                            width: 80px;
+                            height: 80px;
+                            border-radius: 50%;
+                            margin: 15px;
+                            color: white;
+                            text-align: center;
+                            font-size: 15px;
+                            line-height: 80px;
+                            cursor: pointer;
+                            box-shadow: 0 3px 20px 3px #0000004d;
+                            flex-shrink: 0;
+                            transition: transform .3s ease-in-out;
 
-                        &:hover {
-                            transform: scale(.9);
+                            &:hover {
+                                transform: scale(.9);
+                            }
+
+                            &:first-child {
+                                background-color: #4b0e8f;
+                            }
+
+                            &:nth-child(2) {
+                                background-color: #f6298f;
+                            }
+
+                            &:last-child {
+                                background-color: #faf6fb;
+                                color: @mainColor;
+                            }
                         }
 
-                        &:first-child {
-                            background-color: #4b0e8f;
-                        }
-
-                        &:nth-child(2) {
-                            background-color: #f6298f;
-                        }
-
-                        &:last-child {
-                            background-color: #faf6fb;
-                            color: @mainColor;
-                        }
                     }
-
                 }
+
             }
 
         }
@@ -232,13 +298,17 @@
         .second-project {
             .travel-agency {
                 background-image: url("../assets/images/myWorks/travel_agency.png");
-                width: 100%;
                 height: 900px;
-                background-attachment: fixed;
-                background-repeat: no-repeat;
                 background-size: contain;
-                box-shadow: 0 1.8vw 4vw -0.7vw rgba(0, 0, 0, 0.2);
-                position: relative;
+
+            }
+        }
+
+        .third-project {
+            .todo-list {
+                background-image: url("../assets/images/myWorks/todo_list.png");
+                height: 800px;
+                background-size: cover;
             }
         }
     }
