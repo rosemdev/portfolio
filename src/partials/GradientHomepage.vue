d12d9029c12c43f5c89b4a0c6b5579fe1a5dea2d
rosemdev <romasemenyshyn@gmail.com>
2025-06-12 23:46:08 +0300

    Updated intro

--- src/partials/GradientHomepage.vue
+++ src/partials/GradientHomepage.vue
@@ -3,10 +3,8 @@
         <div class="main-content intro" slot="gradientContent">
             <h1>Hello! I'm rosem</h1>
             <h2>And this is my porfolio!</h2>
-            <p>I am a meticulous web developer with 1  year of Salesforce Commerce Cloud development experience and over 5 years of QA experience. 
-                I'm working in the e-commerce sphere, and love it so much. But in my free time, I like to create contemporary, fascinating, and cool websites using new technologies. 
-                So welcome to the portfolio, this is my first and the biggest project on VueJS. Feel free to
-                contact me if you have questions or just to talk! Let's go!</p>
+            <p>I’m a dedicated web developer with over 4 years of experience in building and maintaining e-commerce solutions across different platforms. With a strong foundation in both frontend and backend development, I bring a detail-oriented mindset shaped by my earlier 5+ years in QA. I’m passionate about e-commerce, but even outside of work, I enjoy creating engaging, modern websites using the latest technologies.</p>
+            <p>This portfolio showcases some of my work — including my first and biggest Vue.js project. Whether you're here out of curiosity or with a project in mind, feel free to reach out. I’m always open to collaboration, conversation, or just geeking out over code. Let’s connect!</p>
             <rosem-theme></rosem-theme>
         </div>
     </rosem-gradient>
