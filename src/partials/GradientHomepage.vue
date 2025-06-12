a1e40887e5b5c328340772859898c26424f2cfb0
rosemdev <romasemenyshyn@gmail.com>
2025-06-13 00:02:54 +0300

    Added space between description

--- src/partials/GradientHomepage.vue
+++ src/partials/GradientHomepage.vue
@@ -4,6 +4,7 @@
             <h1>Hello! I'm rosem</h1>
             <h2>And this is my porfolio!</h2>
             <p>I’m a dedicated web developer with over 4 years of experience in building and maintaining e-commerce solutions across different platforms. With a strong foundation in both frontend and backend development, I bring a detail-oriented mindset shaped by my earlier 5+ years in QA. I’m passionate about e-commerce, but even outside of work, I enjoy creating engaging, modern websites using the latest technologies.</p>
+            <br>
             <p>This portfolio showcases some of my work — including my first and biggest Vue.js project. Whether you're here out of curiosity or with a project in mind, feel free to reach out. I’m always open to collaboration, conversation, or just geeking out over code. Let’s connect!</p>
             <rosem-theme></rosem-theme>
         </div>
